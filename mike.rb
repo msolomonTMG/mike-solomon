d9ba876cd5ba31adc081956f7b97434ddd264f0a
Mike Solomon <msolomon@thrillist.com>
2015-07-20 21:32:44 -0400

    change api endpoint documentation

--- mike.rb
+++ mike.rb
@@ -23,7 +23,7 @@ get '/api/documentation' do
 	erb :api_documentation
 end
 
-get '/api/v1/all' do
+get '/v1/all' do
 	experience = get_experience
 	skills = get_skills
 	education = get_education
@@ -31,24 +31,24 @@ get '/api/v1/all' do
 		{ :experience => experience, :skills => skills, :education => education}.to_json
 end
 
-get '/api/v1/status' do
+get '/v1/status' do
 	content_type :json
 		{ :status => true }.to_json
 end
 
-get '/api/v1/experience' do
+get '/v1/experience' do
 	experience = get_experience
 	content_type :json
 		{ :experience => experience}.to_json
 end
 
-get '/api/v1/skills' do
+get '/v1/skills' do
 	skills = get_skills
 	content_type :json
 		{ :skills => skills}.to_json
 end
 
-get '/api/v1/education' do
+get '/v1/education' do
 	education = get_education
 	content_type :json
 		{ :education => education}.to_json
